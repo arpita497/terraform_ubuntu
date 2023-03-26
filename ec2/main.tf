c0826a6149ff711f1683abb5572530719bfaca4b
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-26 22:27:18 +0530

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,18 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
+
+  name = "terrDemo-instance"
+
+  ami                    = "ami-0f8ca728008ff5af4"
+  instance_type          = "t2.micro"
+  key_name               = "new-keypair"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0e7a78055014d406a"]
+  subnet_id              = "subnet-046826bb5e37ac442"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
