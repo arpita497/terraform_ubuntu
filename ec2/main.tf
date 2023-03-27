ea59b01dee0aeb49a36a5e7d345db540fad9cbc1
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-27 12:47:13 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "4.3.0"
 
-  name = "newterr-instance"
+  name = "new-instance"
 
   ami                    = "ami-0f8ca728008ff5af4"
   instance_type          = "t2.micro"
