3cc2f7656e60988ed0701cea06914129671dc7c1
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-27 12:46:59 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,17 +2,17 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "4.3.0"
 
-  name = "terrDemo1-instance"
+  name = "newterr-instance"
 
   ami                    = "ami-0f8ca728008ff5af4"
   instance_type          = "t2.micro"
   key_name               = "new-keypair"
   monitoring             = true
-  vpc_security_group_ids = ["sg-0e7a78055014d406a"]
-  subnet_id              = "subnet-046826bb5e37ac442"
+  vpc_security_group_ids = ["sg-041fb7149ed0c6df5"]
+  subnet_id              = "subnet-0834a179c540198ea"
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = "pre-prod"
   }
 }
