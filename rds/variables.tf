69ab7ca35ea2220f9250a5fb37646cdd0f08e360
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-28 13:55:16 +0530

    Update variables.tf

--- rds/variables.tf
+++ rds/variables.tf
@@ -3,3 +3,15 @@ variable "multi_az" {
         type = bool
         default = true
 }
+
+variable "family" {
+        description = "specifies the family"
+        type = string
+        default = "mysql5.7"
+}
+
+variable "major_engine_version" {
+        description = "DB option group"
+        type = string
+        default = 5.7
+}
